ef94d66f6d46b5b31aea99e6c306f1c8c7f86584
zageabb <zageabb@users.noreply.github.com>
2026-09-16 16:13:25 +0100

    feat: add WhisperDesk launcher

---
+++ run.py
@@ -0,0 +1,13 @@
+from app import create_app
+
+
+app = create_app()
+
+
+if __name__ == "__main__":
+    app.run(
+        host=app.config["HOST"],
+        port=app.config["PORT"],
+        debug=False,
+        threaded=True,
+    )
